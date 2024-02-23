87e4776ec91228ea9d98f34e987a455fb641e7cc
AJAIDEV <146339839+Ajaidev-s@users.noreply.github.com>
2024-02-23 21:13:46 +0530

    Update patternfinal.py

--- technical/patternfinal.py
+++ technical/patternfinal.py
@@ -10,3 +10,16 @@ def pattern(n,down):
     print(n,end=" ")
 n=int(input("enter the number"))
 pattern(n,n)
+
+"""
+insert a  value n
+then print 0 1 2 3 ... n .... 3 2 1 0
+
+constrains:
+1,only one operator
+2,only one condiction
+3,no zero in code
+4,only use 1 and n
+5, no range function
+
+"""
