956d83e4c6096111ef992208f33fb73398dd2b60
AJAIDEV <146339839+Ajaidev-s@users.noreply.github.com>
2024-05-02 06:34:20 +0530

    Update p17.sh

--- ACN/exm2/p17.sh
+++ ACN/exm2/p17.sh
@@ -1,16 +1,1 @@
-echo "limit:"
-read limit
-#read array
-for((i=0;i<limit;i++))
-do
-echo "enter the elements$((i+1)):"
-read a[i]
-done
-#sort elements
-sorted=($(sort <<< "${a[*]}"))
-#sorted
-echo "The sorted array is:"
-for element in "${sorted[@]}"
-do
-    echo "$element"
-done
+
